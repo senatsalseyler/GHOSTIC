432241e424c4c58c5048500697985186cad9a269
Sena <senatunc00@gmail.com>
2022-10-02 14:04:27 +0300

    Initial commit

---
+++ Assets/character_movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class character_movement : MonoBehaviour
+{
+    public GameObject player;
+    public CharacterController controller;
+    public float speed;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        player.transform.position = new Vector2(x * speed , y * speed);
+
+
+        controller.Move(player.transform.position * speed * Time.deltaTime);
+    }
+}
