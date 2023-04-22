ee1129a05168d6e1a03db3dde9090e9a94af543a
senatsalseyler <79267854+senatsalseyler@users.noreply.github.com>
2023-04-22 13:46:07 +0300

    enemy follow improved

--- Assets/Scripts/EnemyFollow.cs
+++ Assets/Scripts/EnemyFollow.cs
@@ -5,19 +5,22 @@ using UnityEngine;
 public class EnemyFollow : MonoBehaviour
 {
     public float speed;
-    public float stoppingDistance;
-    private Transform target;
-
-    void Start()
-    {
-        target = GameObject.FindGameObjectWithTag("ghost").GetComponent<Transform>();
-    }
+    public float distanceBetween;
+    private GameObject target;
+    private float distance;
 
     void Update()
     {
-        if(Vector2.Distance(transform.position, target.position) > stoppingDistance)
+        target = GameObject.FindGameObjectWithTag("ghost");
+        distance = Vector2.Distance(transform.position, target.transform.position);
+        Vector2 direction = target.transform.position - transform.position;
+        direction.Normalize();
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        if(distance < distanceBetween)
         {
-            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+            transform.position = Vector2.MoveTowards(this.transform.position, target.transform.position, speed * Time.deltaTime);
+            transform.rotation = Quaternion.Euler(Vector3.forward * angle);
         }
     }
+
 }
