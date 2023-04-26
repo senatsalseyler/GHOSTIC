841fdb0ed57e7734e18a8c5247a884d2ea3dd3bb
Emre <emrebadem_@hotmail.com>
2023-04-26 12:01:16 +0300

    add smash animation to spiders

--- Assets/Scripts/Spider.cs
+++ Assets/Scripts/Spider.cs
@@ -4,33 +4,25 @@ using UnityEngine;
 
 public class Spider : MonoBehaviour
 {
-    public GameObject spider;
     public AudioSource hit;
-    public GameObject boss_spider;
-    private const float DOUBLE_CLICK_TIME = .5f;
+    public Animator animator;
+    public BoxCollider2D collider;
     private float lastClickTime;
 
     //    public Animation smashAnimation;
     //    bool isDestroyStarted = false;
+    private void Start()
+    {
+        collider = gameObject.GetComponent<BoxCollider2D>();
+    }
+
+    
     void OnMouseDown()
     {
-        spider = GameObject.FindGameObjectWithTag("spider");
-        boss_spider = GameObject.FindGameObjectWithTag("boss_spider");
-
-        float timeSinceLastClick = Time.time - lastClickTime;
-
-        if (timeSinceLastClick <= DOUBLE_CLICK_TIME)
-        {
-            Destroy(boss_spider, 0.15f);
-        }
-
-        else
-        {
-            hit.Play();
-            Destroy(spider, 0.1f);
-        }
-
-        lastClickTime = Time.time;
+        hit.Play();
+        animator.Play("smash");
+        collider.enabled = false;
+        Destroy(gameObject, 0.3f);
     }
 
 
---
+++ Assets/Scripts/SpiderBoss.cs
@@ -0,0 +1,29 @@
+using System;
+using UnityEngine;
+
+public class SpiderBoss : MonoBehaviour
+{
+    public Animator animator;
+    private BoxCollider2D collider;
+    private const float DOUBLE_CLICK_TIME = .5f;
+    private float lastClickTime;
+
+    //    public Animation smashAnimation;
+    //    bool isDestroyStarted = false;
+    private void Start()
+    {
+        collider = gameObject.GetComponent<BoxCollider2D>();
+    }
+
+    void OnMouseDown()
+    {
+        float timeSinceLastClick = Time.time - lastClickTime;
+
+        if (timeSinceLastClick <= DOUBLE_CLICK_TIME)
+        {
+            animator.Play("smash");
+            Destroy(gameObject, 0.3f);
+        }
+        lastClickTime = Time.time;
+    }
+}
