62a2cf5e802c19d57d6b1e2b14634eb555fd56a0
Emre <emrebadem_@hotmail.com>
2023-04-27 01:04:35 +0300

    add leaderboard script

---
+++ Assets/Scripts/Entities/LeaderboardUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class LeaderboardUser
+{
+    public LeaderboardUser(int score, string userId, DateTime timestamp)
+    {
+        this.user_id = userId;
+        this.score = score;
+        this.timestamp = timestamp;
+    }
+
+    public string user_id { get; set; }
+
+    public int score { get; set; }
+
+    public DateTime timestamp { get; set; }
+}
---
+++ Assets/Scripts/Leaderboard.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnityEngine;
+using Firebase.Firestore;
+using Firebase;
+using Firebase.Extensions;
+
+
+public class Leaderboard : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    async Task<List<LeaderboardUser>> getGlobalLeaderboard()
+    {
+        FirebaseFirestore db = FirebaseFirestore.DefaultInstance;
+        CollectionReference colRef = db.Collection("leaderboard");
+        Query leaderboardQuery = colRef.OrderBy("account_id").OrderByDescending("score");
+        QuerySnapshot leaderboardQuerySs = await leaderboardQuery.GetSnapshotAsync();
+        var leaderboardUsers = new List<LeaderboardUser>();
+        foreach (DocumentSnapshot documentSnapshot in leaderboardQuerySs.Documents)
+        {
+            var documentDict = documentSnapshot.ToDictionary();
+            var json = JsonUtility.ToJson(documentDict);
+            LeaderboardUser leaderboardUser = JsonUtility.FromJson<LeaderboardUser>(json);
+            leaderboardUsers.Append(leaderboardUser);
+        }
+
+        return leaderboardUsers;
+    } 
+    
+    async Task<List<LeaderboardUser>> getPersonalLeaderboard()
+    {
+        FirebaseFirestore db = FirebaseFirestore.DefaultInstance;
+        CollectionReference colRef = db.Collection("leaderboard");
+        Query leaderboardQuery = colRef.WhereEqualTo("account_id","").OrderByDescending("score");
+        QuerySnapshot leaderboardQuerySs = await leaderboardQuery.GetSnapshotAsync();
+        var leaderboardUsers = new List<LeaderboardUser>();
+        foreach (DocumentSnapshot documentSnapshot in leaderboardQuerySs.Documents)
+        {
+            var documentDict = documentSnapshot.ToDictionary();
+            var json = JsonUtility.ToJson(documentDict);
+            LeaderboardUser leaderboardUser = JsonUtility.FromJson<LeaderboardUser>(json);
+            leaderboardUsers.Append(leaderboardUser);
+        }
+
+        return leaderboardUsers;
+    }
+}
